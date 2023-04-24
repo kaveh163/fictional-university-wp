5e798f184ca635601897d29cac4e2d64ce00e971
Kaveh <kavehfrounchi@gmail.com>
2023-04-24 11:36:42 +0330

    finished adding custom authorName field to restapi for posts of type post. Working on adding a custom restapi url with get method.

--- wp-content/themes/fictional-university-theme/build/index.asset.php
+++ wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '473970f44c72f44d1494');
+<?php return array('dependencies' => array('jquery'), 'version' => '6487ad10ad0376f0cf40');
