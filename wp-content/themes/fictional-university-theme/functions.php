ac58d73d8a5702c17716730300e2fce3759aa92f
Kaveh <kavehfrounchi@gmail.com>
2023-05-03 07:39:52 +0330

    updated wordpress login page and styling

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -129,4 +129,46 @@ function universityMapKey($api) {
 }
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 
-    ?>
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+//wp_loaded fires when the plugins and theme have loaded.
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+// Customize login screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+function ourHeaderUrl() {
+    return esc_url(site_url('/'));
+}
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+// custom css for login page
+function ourLoginCSS() {
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+
+}
+// login_headertitle deprecated. use login_headertext instead
+add_filter('login_headertext', 'ourLoginTitle');
+function ourLoginTitle() {
+    //if building a theme that might be used by many sites in that case fetch the official sitename from the databas
+    return get_bloginfo('name');
+}
+?>
