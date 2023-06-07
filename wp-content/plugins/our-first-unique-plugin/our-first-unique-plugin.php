f3c166b897f2a06e375db7b8ad1384f6b238ae79
Kaveh <kavehfrounchi@gmail.com>
2023-06-07 09:23:16 +0330

    Finished translation for the the word count plugin

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -6,8 +6,11 @@
  Version: 1.0
  Author: Kaveh
  Author URI: https://www.udemy.com/user/bradschiff/
+ Text Domain: wcpdomain
+ Domain Path: /languages
 */
 
+// Uses wordpress generated form builder. wordpress will handle automatically nonce, security issues, the form submission.
 
 // Plugin is for leveraging action and filter hooks
 // that wordpress makes available to us.
@@ -24,25 +27,67 @@ class WordCountAndTimePlugin
         // admin_init: triggered before any other hook when a user accesses the admin area.
         // used for adding register_setting function
         add_action('admin_init', array($this, 'settings'));
+        add_filter('the_content', array($this, 'ifWrap'));
+        //below: init Fires on the initialization of WordPress
+        add_action('init', array($this, 'languages'));
     }
-    function settings() {
+    //For translation use the loco translation plugin
+    // loco translate looks for the code with __();
+    function languages()
+    {   
+        // load the translated files into languages folder
+        // third argument: path to our languages folder
+        load_plugin_textdomain('wcpdomain', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
+    }
+    function ifWrap($content)
+    {
+        if (is_main_query() and is_single() and (get_option('wcp_wordcount', '1') or get_option('wcp_charactercount', '1') or get_option('wcp_readtime', '1'))) {
+            return $this->createHTML($content);
+        }
+        return $content;
+    }
+    function createHTML($content)
+    {
+        $html = '<h3>' . esc_html(get_option('wcp_headline', 'Post Statistics')) . '</h3><p>';
+        // get word count once because both word count and read time will need it.
+        if (get_option('wcp_wordcount', '1') or get_option('wcp_readtime', '1')) {
+            $wordCount = str_word_count(strip_tags($content));
+        }
+        if (get_option('wcp_wordcount', '1')) {
+            $html .= esc_html__('This post has', 'wcpdomain') . ' ' . $wordCount . ' ' . __('words', 'wcpdomain') . '.<br>';
+        }
+        if (get_option('wcp_charactercount', '1')) {
+            $html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br>';
+        }
+        // Average adult reads 200 or approximately 225 words per minute.
+        if (get_option('wcp_readtime', '1')) {
+            $html .= 'This post will take about ' . round($wordCount / 225) . ' minute(s) to read.<br>';
+        }
+        $html .= '</p>';
+        if (get_option('wcp_location', '0') == '0') {
+            return $html . $content;
+        }
+        return $content . $html;
+    }
+    function settings()
+    {
         // execute 3
         //wcp: word count plugin
         // below: first argument: section name
         // second argument: title for the section (subtitle);
         // third argument: generic html content we want at top of section
-        add_settings_section('wcp_first_section',null, null,'word-count-settings-page');
+        add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
 
-        add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'),'word-count-settings-page','wcp_first_section');
+        add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
         register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
 
-        add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'),'word-count-settings-page','wcp_first_section');
+        add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
         register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
 
-        add_settings_field('wcp_wordcount', 'Word count', array($this, 'checkboxHTML'),'word-count-settings-page','wcp_first_section',array('theName' => 'wcp_wordcount') );
+        add_settings_field('wcp_wordcount', 'Word count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_wordcount'));
         register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
 
-        add_settings_field('wcp_charactercount', 'Character count', array($this, 'checkboxHTML'),'word-count-settings-page','wcp_first_section',array('theName' => 'wcp_charactercount') );
+        add_settings_field('wcp_charactercount', 'Character count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_charactercount'));
         register_setting('wordcountplugin', 'wcp_charactercount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
 
         // execute 2
@@ -52,7 +97,7 @@ class WordCountAndTimePlugin
         // third argument: callback function to output the html
         // fourth argument: slug of the settings page
         // fifth argument: section that we want to add this field to.
-        add_settings_field('wcp_readtime', 'Read Time', array($this, 'checkboxHTML'),'word-count-settings-page','wcp_first_section', array('theName' => 'wcp_readtime') );
+        add_settings_field('wcp_readtime', 'Read Time', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_readtime'));
         // execute 1
         //First argument: the name of the group this setting belongs to
         //Second argument: the actual name for this setting
@@ -61,9 +106,10 @@ class WordCountAndTimePlugin
         //default: if no values exist in database, what would be the default
         register_setting('wordcountplugin', 'wcp_readtime', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
     }
-    // Validation
-    function sanitizeLocation($input) {
-        if($input !='0' AND $input !='1') {
+    // Validation for wcp_location
+    function sanitizeLocation($input)
+    {
+        if ($input != '0' and $input != '1') {
             add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or end.');
             return get_option('wcp_location');
         }
@@ -82,18 +128,21 @@ class WordCountAndTimePlugin
     */
 
     // reusable checkbox function
-    function checkboxHTML($args) { ?>
-        <input type="checkbox" name="<?php echo $args['theName']?>" value="1" <?php checked(get_option($args['theName']), '1')?>>
+    function checkboxHTML($args)
+    { ?>
+        <input type="checkbox" name="<?php echo $args['theName'] ?>" value="1" <?php checked(get_option($args['theName']), '1') ?>>
+        <?php }
+    function headlineHTML()
+    { ?>
+        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')); ?>">
     <?php }
-    function headlineHTML() {?>
-        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline'));?>">
-    <?php }
-    function locationHTML() {?>
+    function locationHTML()
+    { ?>
         <!-- name should match the setting we have just registered -->
-        <select name="wcp_location" >
+        <select name="wcp_location">
             <!-- selected function outputs select attribute for the html -->
-            <option value="0" <?php selected(get_option('wcp_location'), '0');?>>Beginning of post</option>
-            <option value="1" <?php selected(get_option('wcp_location'), '1');?>>End of post</option>
+            <option value="0" <?php selected(get_option('wcp_location'), '0'); ?>>Beginning of post</option>
+            <option value="1" <?php selected(get_option('wcp_location'), '1'); ?>>End of post</option>
         </select>
     <?php }
     function adminPage()
@@ -105,7 +154,7 @@ class WordCountAndTimePlugin
         // manage_options: Only if the user (Admin) has permissions to change options in wordpress should they be able to see this page
         // Fouth argument: The slug for this page. Should be unique.
         // Fifth argument: Function to Output Html content to the new page
-        add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+        add_options_page('Word Count Settings', __('Word Count', 'wcpdomain'), 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
 
     }
     function ourHTML()
@@ -114,7 +163,7 @@ class WordCountAndTimePlugin
         <div class="wrap">
             <h1>Word Count Settings</h1>
             <form action="options.php" method="POST">
-                <?php 
+                <?php
                 // below: output the hidden html for us. the nonce value, action value. handles the security and permission aspects for us
                 settings_fields('wordcountplugin');
                 // wordpress will look for any sections that have been registered with the slug for setting page.
