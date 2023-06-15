a8de063a5e78c5d6b134d47a041d1d1acb8591c8
Kaveh <kavehfrounchi@gmail.com>
2023-06-15 10:00:55 +0330

    made are-you-paying-attention block dynamic.

--- wp-content/plugins/are-you-paying-attention/index.php
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -11,14 +11,34 @@
 if (!defined('ABSPATH'))
     exit; // Exit if accessed directly
 
-class AreYouPayingAttention {
-    function __construct() {
-        // load javascript file when we are in the block editor screen
-        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+class AreYouPayingAttention
+{
+    function __construct()
+    {
+        // register block type with php
+        add_action('init', array($this, 'adminAssets'));
     }
-    function adminAssets() {
-        // add the wp-element dependency just to be sure 100% that it is loaded and exists before wordpress loads our file
-        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+    function adminAssets()
+    {
+        // register a script
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+        register_block_type(
+            'ourplugin/are-you-paying-attention',
+            array(
+                // editor_script: telling wordpress which javascript file to load for this block type
+                'editor_script' => 'ournewblocktype',
+                // function that is responsible to return html that will be seen on the front end of website
+                'render_callback' => array($this, 'theHTML')
+            )
+        );
+    }
+    // First argument is block attributes
+    function theHTML($attributes)
+    {
+        // ob stands for output buffer. anything between ob_start() and ob_get_clean() will be returned
+        ob_start();?>
+        <h3>Today the sky is <?php echo esc_html($attributes['skyColor']);?> and the grass is <?php echo esc_html($attributes['grassColor']);?></h3>
+        <?php return ob_get_clean();
     }
 }
 $areYouPayingAttention = new AreYouPayingAttention();
